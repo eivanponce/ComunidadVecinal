7cd77d595289e73c7fc84a857fa7fdf17c49e74d
eivanponce <ivan.ponce.lm@gmail.com>
2025-04-01 20:25:56 +0200

    version 1.0.3

---
+++ MVVM/Models/TurnosModel.cs
@@ -0,0 +1,33 @@
+﻿using ComunidadVecinal.Helpers;
+using Humanizer;
+using PropertyChanged;
+
+namespace ComunidadVecinal.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class TurnosModel 
+    {
+        public string Semana { get; set; } = string.Empty;
+        public string Piso { get; set; } = string.Empty;
+        public string Width { get; set; } = string.Empty;
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public Color Color { get; set; } = Color.FromArgb("D5F5E3");
+
+        public string FechaInicioModificada
+        {
+            get
+            {
+                return FechaInicio.Humanize();
+            }
+        }
+        public string FechaFinModificada
+        {
+            get
+            {
+                return FechaFin.Humanize();
+            }
+        }
+
+    }
+}
